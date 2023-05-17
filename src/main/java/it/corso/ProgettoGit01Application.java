34484d6602dc61ae7e47e133e052d27adb6f37c1
Emacost96 <costantiniemanuele96@gmail.com>
2023-05-17 12:46:56 +0200

    Initial commit

---
+++ src/main/java/it/corso/ProgettoGit01Application.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoGit01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoGit01Application.class, args);
+	}
+
+}
